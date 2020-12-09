d9c35653fe7d18eb4bbdb11f43e4469d039a24e4
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-12-09 10:55:54 +0100

    Add link generator

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.resolve(public + '/index.html'))
 });
+
+app.get('*', (req, res) => {
+    res.send('404');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
