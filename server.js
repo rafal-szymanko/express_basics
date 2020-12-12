19ea8830c22e7d9e086971d0c858b116d67a7788
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-12-12 21:04:46 +0100

    Add routing

--- server.js
+++ server.js
@@ -3,13 +3,31 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(public + '/index.html'))
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/static/index.html'));
+// });
+
+// app.get('*', (req, res) => {
+//     res.send('404');
+// })
+
+app.get('/:id', (req, res) => {
+  console.log(`Pobranie użytkownika o id ${req.params.id}`)
+})
+
+
+app.post('/:id', (req, res) => {
+  console.log(`Dodawanie użytkownika o id ${req.params.id}`);
 });
 
-app.get('*', (req, res) => {
-    res.send('404');
-})
+app.put('/:id', (req, res) => {
+  console.log(`Zmiana użytkownika o id ${req.params.id}`);
+});
+
+app.delete('/:id', (req, res) => {
+  console.log(`Usuwanie użytkownika o id ${req.params.id}`);
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
